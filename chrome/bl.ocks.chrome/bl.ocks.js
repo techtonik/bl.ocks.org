e0b7eba22a98e9749b5b2fa9696c165137ea63b0
Mike Bostock <mbostock@gmail.com>
2012-08-01 12:30:33 -0700

    Fix display of versioned gists.

--- chrome/bl.ocks.chrome/bl.ocks.js
+++ chrome/bl.ocks.chrome/bl.ocks.js
@@ -1,17 +1,16 @@
-var reGist = /^https?\:\/\/gist\.github\.com\/(\d*)/i,
-    reRel = /^\/?(\d+)$/,
-    gist = reGist.test(location.href),
+var reGist = /^https?\:\/\/gist\.github\.com\/([0-9]+(?:\/[0-9a-f]{40})?)$/,
+    reRel = /^\/?([0-9]+(?:\/[0-9a-f]{40})?)$/,
     anchors = document.querySelectorAll("a[href]"),
     anchor,
     image,
     imageURL = chrome.extension.getURL("bl.ocks.png"),
     i = -1,
-    n = anchors.length,
+    n = (location.hostname !== "bl.ocks.org") && anchors.length,
     href,
     match;
 while (++i < n) {
   match = (href = (anchor = anchors[i]).getAttribute("href")).match(reGist);
-  if (gist && !(match && match[1])) match = href.match(reRel);
+  if ((location.hostname === "gist.github.com") && !(match && match[1])) match = href.match(reRel);
   if (match && match[1]) {
     anchor = anchor.appendChild(document.createElement("a"));
     anchor.setAttribute("href", "http://bl.ocks.org/" + match[1]);
