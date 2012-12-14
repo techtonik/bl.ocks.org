28da3e711c7ab729ccdf217e842ca7d447f8e2c0
Mike Bostock <mbostock@gmail.com>
2012-12-14 14:24:44 -0800

    Better Chrome extension.

--- chrome/bl.ocks.chrome/blocks.js
+++ chrome/bl.ocks.chrome/blocks.js
@@ -2,8 +2,16 @@ redraw();
 document.addEventListener("DOMSubtreeModified", redraw);
 
 function redraw() {
+  var root = document.querySelector(".root-pane");
+  if (!root) return;
+
   var ul = document.querySelector(".export-references");
-  if (!ul) return;
+  if (!ul) {
+    ul = document.createElement("ul");
+    ul.className = "export-references";
+    root.appendChild(ul);
+  }
+
   var a = document.querySelector("#bl-ocks-org");
   if (!a) {
     var li = document.createElement("li");
@@ -11,5 +19,9 @@ function redraw() {
     a = li.firstChild;
     ul.insertBefore(li, ul.firstChild);
   }
-  a.href = "http://bl.ocks.org" + location.pathname.replace(/\/revisions$/, "");
+
+  var parts = location.pathname.substring(1).split("/"), id = parts[0], sha = parts[1];
+  if (!/^([-\w]+|[0-9]+|[0-9a-f]{40})$/.test(id)) return;
+  if (!/^[0-9a-f]{40}$/.test(sha)) sha = null;
+  a.href = "http://bl.ocks.org/" + id + (sha ? "/" + sha : "");
 }
